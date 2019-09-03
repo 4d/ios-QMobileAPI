8103e90d7763109d8b61743f7ec432bb4409f550
Eric Marchand <eric.marchand@4d.com>
2019-09-03 07:15:33 +0100

    [Mobile] Update SwiftPM files to use only iOS and update DeviceKit to 2.1.0 #107928
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/QMobileAPI/": change = 241082]

--- Package.swift
+++ Package.swift
@@ -6,29 +6,20 @@ import PackageDescription
 let package = Package(
     name: "QMobileAPI",
     platforms: [
-        .macOS(.v10_14),
-        .iOS(.v9)
+        .iOS(.v12)
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "QMobileAPI",
-            targets: ["QMobileAPI"]),
+        .library(name: "QMobileAPI", targets: ["QMobileAPI"])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Alamofire/Alamofire.git" , from: "4.8.2"),
-        .package(url: "https://github.com/Moya/Moya.git" , from: "13.0.1"),
-        .package(url: "https://github.com/DaveWoodCom/XCGLogger.git" , from: "7.0.0"),
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git" , from: "5.0.0"),
-        .package(url: "https://github.com/phimage/Prephirences.git" , .revision("HEAD"))
-
-//        .package(url: "https://github.com/devicekit/DeviceKit.git" , .revision("89452446badb4391899e989b8ae99c84488457f5")), // not for macOS
-
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .revision("HEAD")),
+        .package(url: "https://github.com/Moya/Moya.git", .revision("HEAD")),
+        .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.0"),
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
+        .package(url: "https://github.com/phimage/Prephirences.git", .revision("HEAD")),
+        .package(url: "https://github.com/devicekit/DeviceKit.git", from: "2.1.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "QMobileAPI",
             dependencies: [
@@ -36,7 +27,8 @@ let package = Package(
                 "Moya",
                 "XCGLogger",
                 "SwiftyJSON",
-                "Prephirences"
+                "Prephirences",
+                "DeviceKit"
             ],
             path: "Sources"),
         .testTarget(
