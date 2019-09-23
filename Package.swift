71a022f093b11773f55cbf1a111d64ddeaa82f55
Eric Marchand <eric.marchand@4d.com>
2019-09-23 12:53:23 +0100

    [Mobile] Update Alamofire & Moya version #108874
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/QMobileAPI/": change = 241851]

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
         .library(name: "QMobileAPI", targets: ["QMobileAPI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-rc.1"),
-        .package(url: "https://github.com/Moya/Moya.git", from: "14.0.0-beta.1"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0-rc.2"),
+        .package(url: "https://github.com/Moya/Moya.git", from: "14.0.0-beta.2"),
         .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.0"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
         .package(url: "https://github.com/phimage/Prephirences.git", .revision("HEAD")),
