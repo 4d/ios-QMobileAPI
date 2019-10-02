b8268e8ffb24f6e0b07de7a9678a74098f9b4be2
Eric Marchand <eric.marchand@4d.com>
2019-10-02 12:09:03 +0100

    [Mobile] Update some third party framework: Prephirences, DeviceKit, KingFisher
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/QMobileAPI/": change = 242332]

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
         .package(url: "https://github.com/Moya/Moya.git", from: "14.0.0-beta.3"),
         .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.0"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
-        .package(url: "https://github.com/phimage/Prephirences.git", .revision("HEAD")),
-        // .package(url: "https://github.com/devicekit/DeviceKit.git", from: "2.1.0")
+        .package(url: "https://github.com/phimage/Prephirences.git", from: "5.1.0"),
+        // .package(url: "https://github.com/devicekit/DeviceKit.git", from: "2.3.0")
     ],
     targets: [
         .target(
