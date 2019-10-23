84ab13985b139e122788bf8524e0ab54817d0d9c
Eric Marchand <eric.marchand@4d.com>
2019-10-23 15:22:05 +0100

    [Mobile] When deleting record use primaryType in predicat instead of String #111870 Add some utility function and test for core data
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/QMobileAPI/": change = 243531]

--- Sources/Model/Structures/Predicates/Key+Predicate.swift
+++ Sources/Model/Structures/Predicates/Key+Predicate.swift
@@ -31,9 +31,36 @@ extension Key {
     }
 
     public func predicate(forDeletedRecord deletedRecord: DeletedRecord) -> NSPredicate {
-        let value = deletedRecord.primaryKey
+        let value: String = deletedRecord.primaryKey
         let lhs = NSExpression(forKeyPath: self.safeName)
-        let rhs = NSExpression(forConstantValue: value)
+
+        var rhs: NSExpression
+        if let attribute = self.attribute, let storageType = attribute.storageType {
+            switch storageType {
+            case .long:
+                rhs = NSExpression(forConstantValue: Int32(value))
+            case .long64, .duration:
+                rhs = NSExpression(forConstantValue: Int64(value))
+            case .number:
+                rhs = NSExpression(forConstantValue: Double(value))
+            case .string:
+                rhs = NSExpression(forConstantValue: value)
+            case .float:
+                rhs = NSExpression(forConstantValue: Float(value))
+            case .bool:
+                rhs = NSExpression(forConstantValue: Bool(value))
+            case .date:
+                if let date = attribute.simpleDate ? value.simpleDate ?? value.dateFromISO8601: value.dateFromISO8601 {
+                    rhs = NSExpression(forConstantValue: date)
+                } else {
+                    rhs = NSExpression(forConstantValue: value)
+                }
+            default:
+                rhs = NSExpression(forConstantValue: value)
+            }
+        } else {
+            rhs = NSExpression(forConstantValue: value)
+        }
         return NSComparisonPredicate(leftExpression: lhs, rightExpression: rhs, modifier: .direct, type: .equalTo, options: [])
     }
 }
