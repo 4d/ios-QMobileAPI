989d16a897c59101eaf072376026f5ed0f6b301e
Eric Marchand <eric.marchand@4d.com>
2020-06-15 18:45:49 +0100

    [Mobile] Update dependencies for 18R4
    [git-p4: depot-paths = "//depot/4eDimension/main/QMobile/QMobileAPI/": change = 252894]

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire.git", .revision("5.0.0")),
         .package(url: "https://github.com/Moya/Moya.git", .revision("14.0.0")),
-        .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.0"),
+        .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.1"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0"),
         .package(url: "https://github.com/phimage/Prephirences.git", from: "5.1.0"),
         .package(url: "https://github.com/phimage/DeviceKit.git", .branch("feature/macos"))
